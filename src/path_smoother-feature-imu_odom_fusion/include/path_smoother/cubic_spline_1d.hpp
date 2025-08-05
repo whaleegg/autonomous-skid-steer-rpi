d0335ad7d9c8d519f3785298976ddc9b015344c2
Bion7232 <90758213+Bion7232@users.noreply.github.com>
2025-08-05 22:43:41 +0900

    add pure-pursuit repo
    testing pure-pursuit - imu fusion ver

---
+++ src/path_smoother-feature-imu_odom_fusion/include/path_smoother/cubic_spline_1d.hpp
@@ -0,0 +1,27 @@
+// CubicSpline1D.hpp
+
+#ifndef CUBIC_SPLINE_1D_H
+#define CUBIC_SPLINE_1D_H
+
+#include <vector>
+#include <Eigen/Dense>
+
+class CubicSpline1D {
+public:
+    std::vector<double> a, b, c, d, x, y;
+    int nx;
+
+    CubicSpline1D() = default; // デフォルトコンストラクタの追加
+    CubicSpline1D(const std::vector<double>& x, const std::vector<double>& y);
+
+    double calc_position(double x_query);
+    double calc_first_derivative(double x_query);
+    double calc_second_derivative(double x_query);
+
+private:
+    Eigen::MatrixXd calc_A(const std::vector<double>& h);
+    Eigen::VectorXd calc_B(const std::vector<double>& h);
+    int search_index(double x_query);
+};
+
+#endif // CUBIC_SPLINE_1D_H
---
+++ src/pure_pursuit_planner-feature-imu_odom_fusion/include/pure_pursuit_planner/pure_pursuit_planner_component.hpp
@@ -0,0 +1,78 @@
+#ifndef PURE_PURSUIT_PLANNER_COMPONENT_HPP
+#define PURE_PURSUIT_PLANNER_COMPONENT_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "sensor_msgs/msg/imu.hpp" 
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+
+class PurePursuitNode : public rclcpp::Node {
+public:
+    PurePursuitNode();
+    std::vector<double> cx;
+    std::vector<double> cy;
+    std::vector<double> cyaw;
+    std::vector<double> ck;
+
+    // path subscribe flag
+    bool path_subscribe_flag = false;
+    bool odom_subscribe_flag = false;
+
+private:
+    void updateControl();
+    std::pair<double, double> purePursuitControl(int& pind);
+    std::pair<int, double> searchTargetIndex();
+    double calcDistance(double point_x, double point_y) const;
+    void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void path_callback(const nav_msgs::msg::Path::SharedPtr msg);
+    void local_obstacle_callback(const visualization_msgs::msg::MarkerArray::SharedPtr msg);
+    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg); 
+    void publishCmd(double v, double w);
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
+    rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr local_obstacle_sub;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr look_ahead_range_pub;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr obstacle_range_pub;
+    
+    
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Time current_time;
+    double x, y, yaw, v, w;
+    int target_ind;
+    int oldNearestPointIndex;
+    double target_vel;
+    double current_vel;
+
+    // check goal dist
+    double goal_threshold = 0.1; //[m]
+
+    // pure pursuit parameter
+    const double k = 0.1; // look forward gain
+    //const double Lfc = 2.0; // [m] look-ahead distance
+    const double Lfc = 0.25; // [m] look-ahead distance
+    const double Kp = 1.0; // speed proportional gain
+    const double dt = 0.1; // [s] time tick
+
+    // cauvature parameter
+    double minCurvature = 0.0;
+    double maxCurvature = 3.0;
+    double minVelocity = 0.1;
+    double maxVelocity = 0.3;
+};
+
+#endif // PURE_PURSUIT_PLANNER_COMPONENT_HPP
